5778327ee3c3898e69f96251764e15466cbce59c
ChamberlinDev <himalayaommet@gmail.com>
2026-05-27 18:04:58 +0000

    Association_procureur_juge_27_05_2026

--- resources/views/procureur/layout/app.blade.php
+++ resources/views/procureur/layout/app.blade.php
@@ -26,7 +26,7 @@
 
                 <div class="container-fluid" id="container-wrapper">
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Tableau de bord</h1>
+                        <!-- <h1 class="h3 mb-0 text-gray-800">Tableau de bord</h1> -->
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="{{ route('accueil.procureur') }}">Accueil</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Tableau de bord</li>
